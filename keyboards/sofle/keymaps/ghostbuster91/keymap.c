5f8179b2c9f92b1464a3bc49f889384a6fc69864
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2022-08-29 22:43:06 +0200

    Swap colemak and QWERTY

--- keyboards/sofle/keymaps/ghostbuster91/keymap.c
+++ keyboards/sofle/keymaps/ghostbuster91/keymap.c
@@ -14,7 +14,7 @@ combo_t key_combos[COMBO_COUNT] = {
 //
 //             Left hand                          Right hand
 // +-------+-------+-------+-------+   +-------+-------+-------+-------+
-// |  Sym  |  LAlt | Shift | Ctrl  |   | Ctrl  | Shift |  LAlt |  Sym  |
+// |       |  LAlt | Shift | Ctrl  |   | Ctrl  | Shift |  LAlt |       |
 // +-------+-------+-------+-------+   +-------+-------+-------+-------+
 // |  Gui  |  RAlt |                                   |  RAlt |  Gui  |
 // +-------+-------+                                   +-------+-------+
@@ -69,6 +69,29 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
+ * COLEMAK DH
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  |  F10 | F11  | F12  |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |QWERTY|   Q  |   W  |   F  |   P  |   B  |                    |   J  |   L  |   U  |   Y  |   ;  |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | ESC  |   A  |  R/◆ |  S/⇧ | T/⎈  |   G  |-------.    ,-------|   M  | N/⎈  | E/⇧  | I/◆  |   O  |  '   |
+ * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
+ * |      | Z/⎇  |  X/◆ |   C  |   D  |   V  |-------|    |-------|   K  |   H  |   ,  | ./◆  | //⎇  |  _   |
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *            |      |      |      |  NAV | / BSPC  /       \Space \  |  SYM |      |      |      |
+ *            |      |      |      |      |/       /         \      \ |  ENT |      |      |      |
+ *            `-----------------------------------'           '------''---------------------------'
+ */
+
+[_COLEMAK_DH] = LAYOUT(
+  KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,     KC_F6,                         KC_F7,    KC_F8,    KC_F9,    KC_F10,    KC_F11,     KC_F12,
+  KC_QWERTY, KC_Q,      KC_W,      KC_F,      KC_P,      KC_B,                          KC_J,     KC_L,     KC_U,     KC_Y,      KC_SCLN,    XXXXXXX,
+  KC_ESC,    KC_A,      HOME_R,    HOME_S,    HOME_T,    KC_G,                          KC_M,     HOME_N,   HOME_E,   HOME_I,    KC_O,       KC_QUOT,
+  XXXXXXX,   HOME_Z,    HOME_X,    KC_C,      KC_D,      KC_V,    KC_MUTE,     XXXXXXX, KC_K,     KC_H,     KC_COMM,  HOME_DOT,  HOME_SLSH,  KC_UNDS,
+                 XXXXXXX, XXXXXXX, XXXXXXX, MO(_NAV), KC_BSPC,    KC_SPC,  LT(_SYM, KC_ENT), XXXXXXX, XXXXXXX, XXXXXXX
+),
+/*
  * QWERTY
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |COLEMA|   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  `   |
@@ -83,36 +106,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
-
 [_QWERTY] = LAYOUT(
   KC_COLEMAK,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_GRV,
   KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
   KC_TAB,   KC_A,   QHOME_S,    QHOME_D,    QHOME_F,    KC_G,                     KC_H,    QHOME_J,    QHOME_K,    QHOME_L, KC_SCLN,  KC_QUOT,
   KC_LSFT,  QHOME_Z,   QHOME_X,    KC_C,    KC_B,    KC_B, KC_MUTE,     XXXXXXX,KC_N,    KC_M, KC_COMM,  QHOME_DOT, QHOME_SLSH,  KC_RSFT,
                  KC_LGUI,KC_LALT,KC_LCTRL, XXXXXXX, KC_ENT,      KC_SPC,  XXXXXXX, KC_RCTRL, KC_RALT, KC_RGUI
-),
-/*
- * COLEMAK DH
- * ,-----------------------------------------.                    ,-----------------------------------------.
- * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  |  F10 | F11  | F12  |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |QWERTY|   Q  |   W  |   F  |   P  |   B  |                    |   J  |   L  |   U  |   Y  |   ;  |      |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | ESC  |   A  |  R/◆ |  S/⇧ | T/⎈  |   G  |-------.    ,-------|   M  | N/⎈  | E/⇧  | I/◆  |   O  |  '   |
- * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
- * |      | Z/⎇  |  X/◆ |   C  |   D  |   V  |-------|    |-------|   K  |   H  |   ,  | ./◆  | //⎇  |  _   |
- * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            |      |      |SWITCH|  SYM | / BSPC  /       \Space \  |  NAV | CAPS |      |      |
- *            |      |      |      |      |/       /         \      \ |  ENT |      |      |      |
- *            `-----------------------------------'           '------''---------------------------'
- */
-
-[_COLEMAK_DH] = LAYOUT(
-  KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,     KC_F6,                         KC_F7,    KC_F8,    KC_F9,    KC_F10,    KC_F11,     KC_F12,
-  KC_QWERTY, KC_Q,      KC_W,      KC_F,      KC_P,      KC_B,                          KC_J,     KC_L,     KC_U,     KC_Y,      KC_SCLN,    XXXXXXX,
-  KC_ESC,    KC_A,      HOME_R,    HOME_S,    HOME_T,    KC_G,                          KC_M,     HOME_N,   HOME_E,   HOME_I,    KC_O,       KC_QUOT,
-  XXXXXXX,   HOME_Z,    HOME_X,    KC_C,      KC_D,      KC_V,    KC_MUTE,     XXXXXXX, KC_K,     KC_H,     KC_COMM,  HOME_DOT,  HOME_SLSH,  KC_UNDS,
-                 XXXXXXX, XXXXXXX, XXXXXXX, MO(_SYM), KC_BSPC,    KC_SPC,  LT(_NAV, KC_ENT), XXXXXXX, XXXXXXX, XXXXXXX
 ),
 /*
  * _SYMBOL
@@ -123,11 +122,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |   !  |   -  |   +  |   =  |   #  |-------.    ,-------|   |  |   :  |   (  |   )  |   ?  | LOCK |
  * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
- * |      |   ^  |   /  |   *  |   \  | ../  |-------|    |-------|   ~  |   $  |   {  |   }  |   @  |      |
+ * |      |   ^  |   /  |   *  |   \  |      |-------|    |-------|   ~  |   $  |   {  |   }  |   @  |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            |      |      |      |  SYM | /Enter  /       \Space \  |  NAV |      |      |      |
- *            |      |      |      |      |/       /         \      \ |      |      |      |      |
- *            `----------------------------------'           '------''---------------------------'
+ *            |      |      |      |  NAV | / BSCP  /       \Space \  |  SYM |      |      |      |
+ *            |      |      |      |      |/       /         \      \ |  ENT |      |      |      |
+ *            `-----------------------------------'           '------''---------------------------'
  */
 
 [_SYM] = LAYOUT(
@@ -147,16 +146,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
  * |      | LGUI | RAlt |      |      |      |-------|    |-------|      |      | PgDn | PgUp |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            |      |      |      |  SYM | / BSPC  /       \Space \  |  NAV |      |      |      |
+ *            |      |      |      |  NAV | / BSPC  /       \Space \  |  SYM |      |      |      |
  *            |      |      |      |      |/       /         \      \ |  ENT |      |      |      |
  *            `-----------------------------------'           '------''---------------------------'
  */
 [_NAV] = LAYOUT(
-  _______,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,                       XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
-  KC_GRV,    KC_1,      KC_2,      KC_3,      KC_4,      KC_5,                          KC_6,      KC_7,      KC_8,      KC_9,      KC_0,      KC_F12,
-  _______,   KC_EXLM,   KC_AT,     KC_HASH,   KC_DLR,    KC_PERC,                       KC_CIRC,   KC_AMPR,   KC_ASTR,   KC_LPRN,   KC_RPRN,   KC_PIPE,
-  _______,   KC_EQL,    KC_MINS,   KC_PLUS,   KC_LCBR,   KC_RCBR, _______,     _______, KC_LBRC,   KC_RBRC,   KC_SCLN,   KC_COLN,   KC_BSLS,   _______,
-                             XXXXXXX, XXXXXXX, XXXXXXX, _______, _______,       _______, _______, _______, _______, _______
+  _______,   _______,   _______,   _______,   _______,   _______,                       _______,   _______,   _______,   _______,   _______,   _______,
+  _______,   _______,   _______,   _______,   _______,   _______,                       _______,   _______,   KC_HOME,   KC_END,    _______,   _______,
+  _______,   _______,   KC_LALT,   KC_LSFT,   KC_LCTRL,  _______,                       _______,   KC_LEFT,   KC_DOWN,   KC_UP,     KC_RIGHT,  _______,
+  _______,   KC_LGUI,   KC_RALT,   _______,   _______,   _______, _______,     _______, _______,   _______,   KC_PGDOWN, KC_PGUP,   _______,   _______,
+                             _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
 ),
 /* NUMBERS
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -168,7 +167,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
  * |      | LGUI | RAlt |  .   |  ,   |      |-------|    |-------|   =  |   1  |   2  |   3  |   0  |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            |      |      |      |  SYM | / BSPC  /       \Space \  |  NAV |      |      |      |
+ *            |      |      |      |  NAV | / BSPC  /       \Space \  |  SYM |      |      |      |
  *            |      |      |      |      |/       /         \      \ |  ENT |      |      |      |
  *            `-----------------------------------'           '------''---------------------------'
  */
