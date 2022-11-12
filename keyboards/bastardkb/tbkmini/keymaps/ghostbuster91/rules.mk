866bfaa22212fb9099ec5bd55a9cdae96eccac02
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2022-11-12 12:10:06 +0100

    first working version

--- keyboards/bastardkb/tbkmini/keymaps/ghostbuster91/rules.mk
+++ keyboards/bastardkb/tbkmini/keymaps/ghostbuster91/rules.mk
@@ -1,3 +1,9 @@
+# MCU name
+MCU = RP2040
+
+# Bootloader selection
+BOOTLOADER = rp2040
+
 # RP2040-specific options
 ALLOW_WARNINGS = yes
 PICO_INTRINSICS_ENABLED = no # ATM Unsupported by ChibiOS.
@@ -16,14 +22,15 @@ RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 
 AUDIO_SUPPORTED = no        # Audio is not supported
-RGB_MATRIX_SUPPORTED = yes  # RGB matrix is supported and enabled by default
-RGBLIGHT_SUPPORTED = yes    # RGB underglow is supported, but not enabled by default
-RGB_MATRIX_ENABLE = yes     # Enable keyboard RGB matrix functionality
-RGB_MATRIX_DRIVER = WS2812
+RGB_MATRIX_SUPPORTED = no  # RGB matrix is supported and enabled by default
+RGBLIGHT_SUPPORTED = no    # RGB underglow is supported, but not enabled by default
+RGB_MATRIX_ENABLE = no     # Enable keyboard RGB matrix functionality
 
 SPLIT_KEYBOARD = yes
+LAYOUTS = split_3x6_3
 
 SERIAL_DRIVER = vendor
 WS2812_DRIVER = vendor
+
 
 CAPS_WORD_ENABLE = yes
