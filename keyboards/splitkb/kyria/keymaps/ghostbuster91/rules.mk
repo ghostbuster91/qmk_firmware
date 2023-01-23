8787f4ba51536af964ea35c317235e3ae622aa09
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2023-01-23 21:58:44 +0100

    [Kyria] Add Achordion

--- keyboards/splitkb/kyria/keymaps/ghostbuster91/rules.mk
+++ keyboards/splitkb/kyria/keymaps/ghostbuster91/rules.mk
@@ -3,3 +3,7 @@ OLED_DRIVER = SSD1306      # Enables the use of OLED displays
 ENCODER_ENABLE = no       # Disable the use of one or more encoders
 RGB_MATRIX_ENABLE = no     # Disable keyboard RGB matrix, as it is enabled by default on rev3
 RGBLIGHT_ENABLE = no      # Disable keyboard RGB underglow
+SRC += features/achordion.c
+CAPS_WORD_ENABLE = yes
+CONSOLE_ENABLE = no         # Console for debug
+
