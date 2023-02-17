0490b0dcc8ca625c4d55295cdf8de446ecf993cd
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2023-02-17 17:04:51 +0100

    [TbkMini] Don't print layer switch info if console is not enabled

--- keyboards/bastardkb/tbkmini/keymaps/ghostbuster91/keymap.c
+++ keyboards/bastardkb/tbkmini/keymaps/ghostbuster91/keymap.c
@@ -363,7 +363,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 layer_state_t layer_state_set_user(layer_state_t state) {
   state = update_tri_layer_state(state, _NAV, _SYM, _ADJ);
+#ifdef CONSOLE_ENABLE
   uprintf("%d layer\n", state);
+#endif
   return state;
 }
 
