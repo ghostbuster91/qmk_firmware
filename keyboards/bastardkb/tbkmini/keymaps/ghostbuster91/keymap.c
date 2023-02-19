864dd00375fb322b4aff1f5851a581c7c38a4f5f
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2023-02-19 09:10:05 +0100

    [TbkMini] Add tab to thumb cluster

--- keyboards/bastardkb/tbkmini/keymaps/ghostbuster91/keymap.c
+++ keyboards/bastardkb/tbkmini/keymaps/ghostbuster91/keymap.c
@@ -41,6 +41,7 @@
 #define BSPC LT(_NAV, KC_BSPC)
 #define DEL LT(_FUN, KC_DEL)
 #define ENTER RSFT_T(KC_ENT)
+#define TAB LT(_NUM, KC_TAB)
 
 #define O_GUI OSM(MOD_LGUI)
 #define O_SFT OSM(MOD_LSFT)
@@ -84,13 +85,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_CLM] = LAYOUT_split_3x6_3(
         //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-        KC_TAB     ,KC_Q     ,KC_W     ,KC_F    ,KC_P    ,KC_B				,KC_J   ,KC_L   ,KC_U    ,KC_Y    ,KC_SCLN ,XXXXXXX,
+        KC_TAB     ,KC_Q     ,KC_W     ,KC_F    ,KC_P    ,KC_B				            ,KC_J   ,KC_L   ,KC_U    ,KC_Y    ,KC_SCLN ,XXXXXXX,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
         KC_ESC     ,HOME_A   ,HOME_R   ,HOME_S  ,HOME_T  ,HOME_G                        ,HOME_M ,HOME_N ,HOME_E  ,HOME_I  ,HOME_O  ,KC_QUOT,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-	XXXXXXX    ,KC_Z     ,KC_X     ,KC_C    ,KC_D    ,KC_V     			,KC_K   ,KC_H   ,KC_COMM ,KC_DOT  ,KC_SLSH ,XXXXXXX  , 
+	    XXXXXXX    ,KC_Z     ,KC_X     ,KC_C    ,KC_D    ,KC_V     			            ,KC_K   ,KC_H   ,KC_COMM ,KC_DOT  ,KC_SLSH ,XXXXXXX  , 
         //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-						                        KC_NMBR, BSPC     ,DEL       ,ENTER    ,SPACE    ,KC_TMUX
+						                          TAB    ,BSPC    ,DEL       ,ENTER     ,SPACE  ,KC_TMUX
         //`--------------------------'  `--------------------------'
 
         ),
@@ -102,7 +103,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
 	    XXXXXXX    ,KC_Z     ,KC_X     ,KC_C    ,KC_F    ,KC_G     			            ,KC_V   ,KC_M   ,KC_COMM ,KC_DOT  ,KC_SLSH ,XXXXXXX  , 
         //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-						                        KC_NMBR,  BSPC   ,DEL       ,ENTER    ,SPACE    ,KC_TMUX
+						                           TAB   ,BSPC     ,DEL       ,ENTER    ,SPACE  ,KC_TMUX
         //                                    `--------------------------'  `--------------------------'
 
         ),
