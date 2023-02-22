edc8962b5d675bc6f740ba004c86a07706bdf535
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2023-02-22 23:10:13 +0100

    [Kyria] Do not apply shift to KC_MINS when in caps mode

--- keyboards/splitkb/kyria/keymaps/ghostbuster91/keymap.c
+++ keyboards/splitkb/kyria/keymaps/ghostbuster91/keymap.c
@@ -309,6 +309,24 @@ bool achordion_chord(uint16_t tap_hold_keycode, keyrecord_t *tap_hold_record,
   return achordion_opposite_hands(tap_hold_record, other_record);
 }
 
+bool caps_word_press_user(uint16_t keycode) {
+    switch (keycode) {
+        // Keycodes that continue Caps Word, with shift applied.
+        case KC_A ... KC_Z:
+            add_weak_mods(MOD_BIT(KC_LSFT));  // Apply shift to next key.
+            return true;
+        // Keycodes that continue Caps Word, without shifting.
+        case KC_1 ... KC_0:
+        case KC_BSPC:
+        case KC_DEL:
+        case KC_UNDS:
+            return true;
+
+        default:
+            return false;  // Deactivate Caps Word.
+    }
+}
+
 #ifdef OLED_ENABLE
 
 bool oled_task_user(void) {
